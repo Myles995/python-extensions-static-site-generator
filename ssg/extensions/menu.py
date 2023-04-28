2dc1b93d57b30908042a754b13e20be0adc810dc
Myles McDermott <cmt_myles@hotmail.com>
2023-04-28 10:31:28 +0930

    3.2

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -2,12 +2,11 @@ from ssg import hooks, parsers
 
 files = []
 
-@hooks.register(collect_files)
+@hooks.register("collect_files")
 def collect_files(source, site_parsers):
     
     valid = lambda p : not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
-        for parser in site_parsers:
-            list(filter(valid, site_parsers))
-            if path.suffix == parser.valid_file_ext():
-                files.append(path)
+        for parser in list(filter(valid, site_parsers)):
+                if path.suffix == parser.valid_file_ext():
+                    files.append(path)
